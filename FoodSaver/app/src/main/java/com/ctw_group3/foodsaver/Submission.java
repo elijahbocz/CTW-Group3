d9664a7f81f0f5cfc3dc20f5be8c38c8acff95e6
Elijah <boczelijah@gmail.com>
2021-06-30 17:35:12 -0400

    upgrade submission activity to support image uploading,
    uses Volley library for the POST request, and uploads to Imgur atm

--- FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/Submission.java
+++ FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/Submission.java
@@ -1,15 +1,33 @@
 package com.ctw_group3.foodsaver;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.basgeekball.awesomevalidation.utility.RegexTemplate;
@@ -20,19 +38,31 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Submission extends AppCompatActivity{
     EditText etName,etMobile,etSName, etAddress, etFoodName, etFoodDesc;
-    Button btSubmit;
+    Button btSubmit, btUploadImage;
+    ImageView foodImage;
+    private String foodImageLink;
+
     AwesomeValidation awesomeValidation;
     private String TAG = "SubmissionActivity";
+    private final static int IMAGE_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         // Ensure the user is signed in
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -44,6 +74,8 @@ public class Submission extends AppCompatActivity{
             etAddress = findViewById(R.id.et_address);
             etFoodName = findViewById(R.id.et_food_name);
             etFoodDesc = findViewById(R.id.et_food_desc);
+            foodImage = findViewById(R.id.food_image);
+            btUploadImage = findViewById(R.id.upload_image_btn);
             btSubmit = findViewById(R.id.bt_submit);
 
             // Validators for the EditBoxes
@@ -54,6 +86,16 @@ public class Submission extends AppCompatActivity{
                     ,"[5-9]{1}[0-9]{9}$",R.string.invalid_number);
             awesomeValidation.addValidation(this,R.id.et_store_name
                     ,RegexTemplate.NOT_EMPTY,R.string.invalid_storename);
+
+            btUploadImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(Intent.createChooser(intent, "Select Image"), IMAGE_CODE);
+                }
+            });
 
             btSubmit.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -107,7 +149,73 @@ public class Submission extends AppCompatActivity{
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (resultCode == RESULT_OK) {
+            // Compare the resultCode with the IMAGE_CODE constant
+            if (requestCode == IMAGE_CODE) {
+                // Get the uri of the image from data
+                // URI is basically pointing to the image stored on the android device
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // Update the preview image in the layout
+                    foodImage.setImageURI(selectedImageUri);
+
+                    try {
+                        // Convert the image pointed at by the URI into a bitmap, then into an array of bytes
+                        // Goal is to convert the image to base64
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImageUri);
+                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                        byte[] byteArray = outputStream.toByteArray();
+
+                        // Convert the byte array to a Base64 String
+                        String imageEncodedString = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                        // Create a body for the HTTP POST request
+                        JSONObject jsonBody = new JSONObject();
+                        try {
+                            jsonBody.put("image", imageEncodedString);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Perform a HTTP POST request using the Volley library to Imgur
+                        String url ="https://api.imgur.com/3/upload/";
+                        // Instantiate the RequestQueue.
+                        RequestQueue queue = Volley.newRequestQueue(this);
+                        // The HTTP request will return a JSON Object
+                        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody, new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.w(TAG, response.toString());
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                onBackPressed();
+                            }
+                        }) {
+                            @Override
+                            public Map<String, String> getHeaders() throws AuthFailureError {
+                                // Headers for the POST request, Imgur requires authorization
+                                final Map<String, String> headers = new HashMap<>();
+                                headers.put("Authorization", "Client-ID dad1b3ce85b1e3f");
+                                return headers;
+                            }
+                        };
+
+                        // Add the request to the RequestQueue.
+                        queue.add(jsonRequest);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
